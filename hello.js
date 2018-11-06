45fe731d13e383aa79e72e7a700941c0765ef60b
Keaton Hawkins <githubuser12@gmail.com>
2018-11-06 10:17:58 -0500

    added four buttons and first JS

---
+++ hello.js
@@ -0,0 +1,18 @@
+function consoleLog(){
+  console.log("Hello, Console");
+}
+
+function popUp(){
+  alert("Hello, World!");
+}
+
+function helloBrowser(){
+  var p = document.getElementById('lolxD');
+  p.innerHTML = "Hello, World!";
+}
+
+function helloStranger(){
+  var name = prompt("Please enter your name");
+  var pg = document.getElementById('urbad');
+  p.innerHTML = "Hello, " + name;
+}
